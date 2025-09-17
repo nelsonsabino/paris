aa58db86242bf9747117d9c372ed713d9fe09b48
nelsonsabino <sabino.nelson@gmail.com>
2025-09-18 00:12:07 +0100

    Create alimentacao_dados.js

---
+++ data/alimentacao_dados.js
@@ -0,0 +1,10 @@
+// data/alimentacao_dados.js
+
+// Valores estimados de ALIMENTAÇÃO por dia
+export const orcamentoAlimentacao = {
+    "2025-09-19": { total4p: 101.92, porPessoa: 25.48 }, // Dia 1
+    "2025-09-20": { total4p: 93.60, porPessoa: 23.40 },  // Dia 2
+    "2025-09-21": { total4p: 119.60, porPessoa: 29.90 }, // Dia 3
+    "2025-09-22": { total4p: 122.20, porPessoa: 30.55 }, // Dia 4
+    "2025-09-23": { total4p: 171.60, porPessoa: 42.90 }  // Dia 5
+};
